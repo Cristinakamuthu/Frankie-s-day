4b8c515cbfc147d2e57e385ff9d6e13edb7b2027
Cristina <kamuthucristina@gmail.com>
2025-07-18 09:54:14 +0300

    added the links to the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <li><NavLink to="/" className="navlink">Home</NavLink></li>
+      <li><NavLink to="/about" className="navlink">About</NavLink></li>
+      <li><NavLink to="/reposts" className="navlink">Reposts</NavLink></li>
+      <li><NavLink to="/channel" className="navlink">Channel</NavLink></li>
+
+
+
+      
+
+    </div>
   )
 }
 
