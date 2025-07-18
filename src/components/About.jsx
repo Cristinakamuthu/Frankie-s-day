0a4e7c8422284634f83607af7a0ad78b8251877e
Cristina <kamuthucristina@gmail.com>
2025-07-18 13:19:20 +0300

    added stuff to the tmt page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import francisca from '/home/cristina/Development/code/my_own_projects/frankie-portfolio/src/francisca.jpeg'
 
 const About = () => {
   return (
@@ -7,6 +8,14 @@ const About = () => {
       <header>
         <Navbar/>
       </header>
+      <div className='about-page'>
+      <div className='image-container' >
+        <img src={francisca} alt='profile' className='profile-pic'></img>
+</div>
+<div>
+  
+</div>
+      </div>
     </div>
   )
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import TMT from '/home/cristina/Development/code/my_own_projects/frankie-portfolio/src/TMT.jpeg'
 
 const Videos = () => {
   return (
@@ -7,6 +8,19 @@ const Videos = () => {
      <header>
         <Navbar/>
       </header> 
+      <div repost-image>
+        <img src={TMT} alt='tmt crew' className='repost-image-container'/>
+  <span className="centered-text">TMT CREW</span>
+      </div>
+      <div className="tmt-crew-thanks animate">
+  <p>
+    TMT Crew is the dedicated team that stands beside Francisca in every season — 
+    from studio sessions to live performances. Their passion, loyalty, and faith 
+    bring her vision to life. She’s deeply grateful for their unwavering support 
+    and the love they pour into every moment.
+  </p>
+</div>
+
     </div>
   )
 }
